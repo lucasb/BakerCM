a21b413642701dc09a1bc4c78d0eea670645aac5
Lucas Boscaini <lucasboscaini@gmail.com>
2018-05-15 10:24:57 -0300

    Add configuration for tests and lint.

--- setup.py
+++ setup.py
@@ -3,19 +3,13 @@ from setuptools import setup
 from baker import __version__
 
 
-def readme():
-    with open('README.rst') as f:
-        return f.read()
-
-
 setup(name='bakercm',
       version=__version__,
       description='Baker is a decentralized configuration management based on files',
-      long_description=readme(),
+      long_description='Baker is a decentralized configuration management based on files',
       classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Topic :: System :: Software Distribution',
@@ -30,7 +24,7 @@ setup(name='bakercm',
           'pycryptodome==3.4.11',
       ],
       test_suite='nose.collector',
-      tests_require=['nose', 'nose-cover3'],
+      tests_require=['nose', 'nose-cover3', 'coverage', 'flake8'],
       entry_points={
           'console_scripts': ['baker=baker:main'],
       },
