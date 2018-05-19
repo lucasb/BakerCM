861cb0fd94ecaa45200c9eaaa34301631cf83a46
Lucas Boscaini <lucasboscaini@gmail.com>
2018-05-18 22:04:52 -0300

    Remove mocked test.

--- tests/test_config.py
+++ tests/test_config.py
@@ -7,13 +7,3 @@ class TestConfig(TestCase):
         output = Popen(['baker', 'configs', '-a'], stdout=PIPE).communicate()
         output = output[0].decode('utf-8')
         self.assertTrue('DEBUG=False' in output)
-
-    @mock.patch('baker.settings.Path.is_file')
-    @mock.patch('baker.settings.Storage.file')
-    def test_list_config(self, mock_path, mock_storage):
-        mock_path.return_value = True
-        mock_storage.return_value = 'DEBUG=True'
-
-        output = Popen(['baker', 'configs'], stdout=PIPE).communicate()
-        output = output[0].decode('utf-8')
-        self.assertTrue('DEBUG=True' in output)
